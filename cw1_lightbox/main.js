aa6fa38393af4a32a630062036ddc0aeca602a22
Dawid Szczurkowski <dszczurkowski@gmail.com>
2020-10-10 18:31:06 +0200

    eslint added

--- cw1_lightbox/main.js
+++ cw1_lightbox/main.js
@@ -12,10 +12,10 @@
 
 
 // pobierz wszystkie grafiki z .gallery
-const imgs = document.querySelectorAll('.gallery img');
+const imgs = document.querySelectorAll(".gallery img");
 for (let index = 0; index < imgs.length; index++) {
     const img = imgs[index];
-    img.addEventListener('click', showLightbox);
+    img.addEventListener("click", showLightbox);
 }
 
 function showLightbox(ev) {
@@ -24,12 +24,12 @@ function showLightbox(ev) {
     // const nextEl = ev.target.nextElementSibling;
     console.log(ev);
     // pobierz elementy z html-a
-    const lightbox = document.querySelector('.lightbox');
-    const img = document.querySelector('.lightbox img');
+    const lightbox = document.querySelector(".lightbox");
+    const img = document.querySelector(".lightbox img");
     // pobierz url klikniętej grafiki
     const imgUrl = ev.target.src;
     // przypisz do grafiki w lightboxie
     img.src = imgUrl;
     // pokaż lightbox
-    lightbox.classList.add('visible');
+    lightbox.classList.add("visible");
 }
