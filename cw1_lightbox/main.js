4cf6f492f92ece0a80f582346eb321d3cba5b754
Dawid Szczurkowski <dszczurkowski@gmail.com>
2020-10-10 23:58:56 +0200

    prawie działajacy przycisk prev/next

--- cw1_lightbox/main.js
+++ cw1_lightbox/main.js
@@ -12,9 +12,11 @@
 
 
 // pobierz wszystkie grafiki z .gallery
+
 const prev = document.querySelector(".prev");
+const next = document.querySelector(".next");
 const imgs = document.querySelectorAll(".gallery img");
-const hide = document.querySelector(".lightbox:not(img)");
+const hide = document.querySelector(".lightbox");
 for (let index = 0; index < imgs.length; index++) {
     const img = imgs[index];
     img.addEventListener("click", showLightbox);
@@ -22,31 +24,40 @@ for (let index = 0; index < imgs.length; index++) {
 
 function showLightbox(ev) {
     // pobranie poprzedniego elementu
-    const prevEl = ev.target.prevElementSibling;
-    // const nextEl = ev.target.nextElementSibling;
-    console.log(ev);
+    
+    const nextEl = ev.target.nextElementSibling;
+    
     // pobierz elementy z html-a
     const lightbox = document.querySelector(".lightbox");
     const img = document.querySelector(".lightbox img");
+    const prevEl = ev.target.previousElementSibling;
+    console.log(nextEl);
     // pobierz url klikniętej grafiki
     const imgUrl = ev.target.src;
     // przypisz do grafiki w lightboxie
     img.src = imgUrl;
     // pokaż lightbox
     lightbox.classList.add("visible");
-    prev.addEventListener("click",prevEl);
-
+    prev.addEventListener("click",function(ev) {
+        
+        img.src = prevEl.src;
+        ev.stopPropagation();
+    });
+    next.addEventListener("click",function(ev) {
+        
+        img.src = nextEl.src;
+        ev.stopPropagation();
+    });
+    img.addEventListener("click", stop);
 }
 
 
 hide.addEventListener("click", hideLightbox);
-prev.addEventListener("click", stop);
+// prev.addEventListener("click", stop);
 function hideLightbox() {
     const lightbox = document.querySelector(".lightbox");
-    lightbox.classList.add("hide");
     lightbox.classList.remove("visible");
-    lightbox.classList.remove("hide");
 }
 function stop(ev){
-    ev.stopPropagation();    
+    ev.stopPropagation();
 }
