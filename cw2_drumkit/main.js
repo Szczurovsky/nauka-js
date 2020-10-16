2ad485068f72820eac72e587feae19b092199de9
Dawid Szczurkowski <dszczurkowski@gmail.com>
2020-10-16 16:43:24 +0200

    hide audio

--- cw2_drumkit/main.js
+++ cw2_drumkit/main.js
@@ -1,7 +1,63 @@
+/* eslint-disable no-duplicate-case */
 document.body.addEventListener("keypress", onKeyPress);
-function onKeyPress(ev){
-    if (ev.code==="KeyA"){
-        const sound = document.querySelector("#boom");
+document.querySelector("#playBtn").addEventListener("click", playChannel);
+const channel = [];
+const recordStart = Date.now();
+function onKeyPress(ev) {
+    let sound;
+    let soundName;
+    switch (ev.code) {
+    case "KeyA":
+        soundName = "boom";
+        sound = document.querySelector("#boom");
+        break;
+    case "KeyF":
+        soundName = "clap";
+        sound = document.querySelector("#clap");
+        break;
+    case "KeyD":
+        soundName = "hihat";
+        sound = document.querySelector("#hihat");
+        break;
+    case "KeyS":
+        soundName = "kick";
+        sound = document.querySelector("#kick");
+        break;
+    }
+    if (sound) {
+        const keyPressTime = Date.now() - recordStart;
+        const recordedSound = {
+            sound: soundName,
+            time: keyPressTime
+        };
+        channel.push(recordedSound);
         sound.play();
     }
+}
+
+
+    
+
+function playChannel() {
+    
+    for (let index = 0; index < channel.length; index++) {
+
+        {
+            const soundObject = channel[index];
+            setTimeout(
+                () => {
+                    playSound(soundObject.sound);
+                },
+                soundObject.time
+               
+            );
+        }
+
+    }
+}
+
+
+function playSound(soundName) {
+    const sound = document.querySelector("#" + soundName);
+    sound.play();
 }
