f6373fd901f372b88c1d1a39ca8ca5172cde2f79
Dawid Szczurkowski <dszczurkowski@gmail.com>
2020-11-23 12:40:46 +0100

    przechodzenie w obiekty

--- projekty_obiektowo/ballinhole/main.js
+++ projekty_obiektowo/ballinhole/main.js
@@ -8,6 +8,7 @@ class Circle {
                 "http://www.w3.org/2000/svg",
                 "circle"
             );
+            
             circle.classList.add("shape");
             svg.appendChild(circle);
         }
@@ -21,8 +22,76 @@ class Circle {
             circle.style.cx = Math.floor(Math.random() * window.innerWidth);
             circle.style.cy = Math.floor(Math.random() * window.innerHeight);
             circle.style.fill = "#" + randomColor;
+            
         }
     }
+    circleColor(){
+        const board = document.querySelectorAll(".shape");
+        for(let i=0;i<board.length;i++){
+            const circle = board[i];
+            this.color = (circle.style.fill);
+        }
+    }
+    circleCords(){
+        const board = document.querySelectorAll(".shape");
+        for(let i=0;i<board.length;i++){
+            const circle = board[i];
+            const coordinates = {
+                X: circle.style.cx,
+                Y: circle.style.cy,
+                color: circle.style.fill,
+            };
+            let cords= [];
+            cords.push(coordinates);
+            console.log(cords);
+        }
+    }
+    circleMove(){
+        const constAX = a.x;
+        ball.style.cx = constAX;
+        let parseBallX=parseInt(ball.style.cx);
+        const constBY = a.y;
+        ball.style.cy = constBY;
+        let parseBallY=parseInt(ball.style.cy);
+
+        setInterval(move, 1);
+        function move() {             
+            let speedX = window.speedX*0.05;
+            let speedY = window.speedY*0.05; 
+            if(isNaN(speedX)){
+                speedX=constAX;}
+            else{
+                parseBallX += speedX;
+                ball.style.cx = parseBallX;
+                parseBallY += speedY;
+                ball.style.cy = parseBallY;
+                let mainCircle={
+                    X: ball.style.cx,
+                    Y: ball.style.cy,
+                };  
+                console.log(mainCircle);
+            }  
+        }   
+    }
+    handleOrientation(event) {
+        let y = event.beta;
+        let x = event.gamma; 
+    
+        x+=0;
+        y+=0;
+    
+        window.speedX = x;
+        window.speedY = y;
+        console.log(window.speedX);
+    }
+}
+
+
+class gameRule{
+    constructor(){
+        this.circle=new Circle();
+    }
+    
 }
 
 const kolo = new Circle();
@@ -52,96 +121,25 @@ function Timer() {
     }, 1000);
 }
 
-// const mainCircle = document.querySelector(".mainShape");
-// const circleData = mainCircle.getBBox();
-// const body = document.querySelector("body");
-// const gameWidth = body.offsetWidth;
-
-// const gameHeight = body.offsetHeight;
-// const maxX = gameWidth - circleData.width;
-// const maxY = gameWidth - circleData.height;
-// console.log(circleData.x);
-// function handleOrientation(event){
-//     let x = event.beta;
-//     let y = event.gamma;
-//     if(x>90) {x = 90;}
-//     if(x <- 90) {x = -90;}
-//     x += 90;
-//     y += 90;
-//     mainCircle.cx = (maxY*y/180 - 10);
-//     console.log(maxX);
-// }
 const garden = document.querySelector("body");
 const ball = document.querySelector(".mainShape");
-// ball.style.r = "10px";
-// ball.style.cx = "50%";
-// ball.style.cy = "50%";
+
 const a = ball.getBBox();
 
-// var output = document.querySelector(".output");
+// handleOrientation(event) {
+//     var y = event.beta;
+//     var x = event.gamma; 
 
-var maxX = garden.clientWidth - ball.clientWidth;
-var maxY = garden.clientHeight - ball.clientHeight;
+//     x+=0;
+//     y+=0;
 
-function handleOrientation(event) {
-    var y = event.beta; // In degree in the range [-180,180]
-    var x = event.gamma; // In degree in the range [-90,90]
-
-    // output.innerHTML  = "beta : " + x + "\n";
-    // output.innerHTML += "gamma: " + y + "\n";
-
-    // Because we don't want to have the device upside down
-    // We constrain the x value to the range [-90,90]
-    // if (x >  90) { x =  90;}
-    // if (x < -90) { x = -90;}
-
-    // To make computation easier we shift the range of
-    // x and y to [0,180]
-    // x += 90;
-    // y += 90;
-    x+=0;
-    y+=0;
-
-    // 10 is half the size of the ball
-    // It center the positioning point to the center of the ball
-    // ball.style.cy  = (maxX*x/180 - 5) + "px";
-    // ball.style.cx = (maxY*y/180 - 5) + "px";
-    window.speedX = x;
-    window.speedY = y;
-    console.log(window.speedX);
-}
-
-
-// ustawienie stalej wartosci polozenia mainShape
-const constAX = a.x;
-ball.style.cx = constAX;
-let parseBallX=parseInt(ball.style.cx);
-const constBY = a.y;
-ball.style.cy = constBY;
-let parseBallY=parseInt(ball.style.cy);
-const menu = document.querySelector(".start");
-function test() {
-    // setInterval(()=>{ball.style.cx += window.speedX;}, 500);
-    // setInterval(()=>{a.x += window.speedX;console.log(a.x);}, 5000);
-    setInterval(move, 1);
-    function move() {
-        
-        let speedX = window.speedX*0.05;
-        let speedY = window.speedY*0.05;
-        // eslint-disable-next-line use-isnan
-        if(isNaN(speedX)){
-            speedX=constAX;}
-        else{
-            parseBallX += speedX;
-            ball.style.cx = parseBallX;
-            parseBallY += speedY;
-            ball.style.cy = parseBallY;
-        }     
-    }
-}
-// function test1(){
-//     setInterval(()=>{ball.style.})
+//     window.speedX = x;
+//     window.speedY = y;
+//     console.log(window.speedX);
 // }
-window.addEventListener("DOMContentLoaded", test);
-window.addEventListener("deviceorientation", handleOrientation);
-// kolo.animateCircle();
+
+const menu = document.querySelector(".start");
+
+window.addEventListener("DOMContentLoaded", kolo.circleMove());
+window.addEventListener("deviceorientation",(ev)=> kolo.handleOrientation(ev));
+
