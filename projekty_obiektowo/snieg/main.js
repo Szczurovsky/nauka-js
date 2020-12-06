513fc2d28f990d4b63ed97a8a3997fd78f2bf42c
Szczurovsky <dszczurkowski@gmail.com>
2020-12-06 14:07:50 +0100

    czyszczenie tablicy balls

--- projekty_obiektowo/snieg/main.js
+++ projekty_obiektowo/snieg/main.js
@@ -3,7 +3,7 @@ const ctx = canvas.getContext("2d");
 class Ball {
     constructor(x) {
         this.x = x;
-        this.y = 0;
+        this.y = this.getRandomNum(1, 100);
     }
 
     getRandomNum(min, max) {
@@ -11,9 +11,9 @@ class Ball {
     }
 
     losuj() {
-        this.radius = this.getRandomNum(10, 20);
+        this.radius = this.getRandomNum(1, 5);
         // this.speedx = this.getRandomNum(10, 15);
-        this.speedy = this.getRandomNum(1, 5);
+        this.speedy = this.getRandomNum(1, 10);
     }
 
     updatePositiony() {
@@ -24,33 +24,38 @@ class Ball {
         // console.log(Math.pow(-1, i));
     }
 }
+const ballek = new Ball();
 function losujPozycje() {
-    return Math.floor(Math.random() * 1000);
+    return Math.floor(Math.random() * (canvas.width - 2));
 }
 let balls = [];
 // const ball = new Ball(losujPozycje());
 // ball.losuj();
+setInterval(DodajKolka, 3000);
+function DodajKolka() {
+    for (let i = 0; i <= 60; i++) {
+        const ball = new Ball(losujPozycje());
+        ball.losuj();
+        balls.push(ball);
+    }
 
-for (let i = 0; i <= 50; i++) {
-    const ball = new Ball(losujPozycje());
-    ball.losuj();
-    balls.push(ball);
+    for (let i = 0; i <= 60; i++) {
+        ctx.beginPath();
+        ctx.fillStyle = "#FFFFFF";
+        ctx.arc(balls[i].x, balls[i].y, balls[i].radius, 0, 2 * Math.PI, false);
+        ctx.fill();
+        ctx.closePath();
+    }
 }
-// draw shape
-for (let i = 0; i <= 50; i++) {
-    ctx.beginPath();
-    ctx.fillStyle = "#FF0000";
-    ctx.arc(balls[i].x, balls[i].y, balls[i].radius, 0, 2 * Math.PI, false);
-    ctx.fill();
-    ctx.closePath();
-}
-
 setInterval(animate, 50);
-setInterval(animate1, 500);
+// setInterval(animate1, 500);
 window.c = 5;
 function animate() {
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < balls.length; i++) {
+        if (balls[i].y >= canvas.height) {
+            balls.splice(i,1);
+        }
         balls[i].updatePositiony();
         ctx.beginPath();
         ctx.arc(balls[i].x, balls[i].y, balls[i].radius, 0, Math.PI * 2, true);
@@ -58,10 +63,17 @@ function animate() {
 
         ctx.closePath();
     }
+    for(let i in balls){
+        if (balls[i].y >= canvas.height) {
+            snow.style.offsetHeight++;
+        }
+    }
 }
+const snow = document.querySelector(".snow");
 function animate1() {
     window.c++;
-    for (let i = 0; i < 1; i++) {
+    for (let i = 0; i < balls.length; i++) {
+      
         balls[i].updatePositionX(window.c);
         ctx.beginPath();
         ctx.arc(balls[i].x, balls[i].y, balls[i].radius, 0, Math.PI * 2, true);
