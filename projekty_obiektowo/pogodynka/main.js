3adc66e8e9ac2d861f71ef58fdbb7d50f3e5775a
Dawid Szczurkowski <dszczurkowski@gmail.com>
2021-01-06 11:17:35 +0100

    działa juz podstawowa ocena

--- projekty_obiektowo/pogodynka/main.js
+++ projekty_obiektowo/pogodynka/main.js
@@ -1,13 +1,16 @@
 class Note{
-    constructor(title,content,pinned,color){
-        this.title = title;
-        this.content = content;
-        this.pinned = pinned;
+    constructor(miasto , description, icon,temperature,pressure, humidity,color){
+        this.miasto = miasto;
+        this.description = description;
+        this.icon = icon;
+        this.temperature = temperature;
+        this.pressure = pressure;
+        this.humidity = humidity;
         this.color = color;
         this.createDate = new Date();
     }
 }
-let title , contents;
+let miasto,description,icon,temperature,pressure, humidity;
 
 class Notes {
     constructor(){
@@ -89,20 +92,43 @@ class UI {
     drawNote(notes){
         for(let note of notes){
             const htmlSection = document.createElement("section");
-            const htmlTitle = document.createElement("h1");
-            const htmlContent = document.createElement("p");
-            const htmlDate = document.createElement("h4");
+            const htmlMiasto = document.createElement("h1");
+            const htmlDescription = document.createElement("h4");
+            const htmlIcon = document.createElement("h4");
+            htmlIcon.innerHTML = note.icon;
+            
+            const htmlTemperature = document.createElement("h4");
+            htmlTemperature.innerHTML = note.temperature;
+          
+
+            const htmlPressure = document.createElement("h4");
+            htmlPressure.innerHTML = note.pressure;
+           
+
+            const htmlHumidity = document.createElement("h4");
+            htmlHumidity.innerHTML = note.humidity;
+            
+            const htmlDate = document.createElement("p");
     
+
+
+
+
+            // (miasto , description, icon,temperature,pressure, humidity, window.color);
             htmlSection.classList.add("note");
-            htmlTitle.innerHTML = note.title;
+            htmlMiasto.innerHTML = note.miasto;
             htmlSection.dataset.id = note.id;
-            htmlContent.innerHTML = note.content;
+            htmlDescription.innerHTML = note.description;
             htmlDate.innerHTML = note.createDate.toLocaleString();
     
-            htmlSection.appendChild(htmlTitle);
-            htmlSection.appendChild(htmlContent);
+            htmlSection.appendChild(htmlMiasto);
+            htmlSection.appendChild(htmlDescription);
+
+            htmlSection.appendChild(htmlIcon);
+            htmlSection.appendChild(htmlTemperature);
+            htmlSection.appendChild(htmlPressure);
+            htmlSection.appendChild(htmlHumidity);
             htmlSection.appendChild(htmlDate);
-        
             const main = document.querySelector("main");
             main.appendChild(htmlSection);
     
@@ -144,7 +170,6 @@ document.querySelector("#noteAdd").addEventListener("click", ()=>{
     // // window.titles = document.querySelector("#noteTitle").value;
     // let miasto = document.querySelector("#noteTitle").value;
     // // window.contents = document.querySelector("#noteContent").value;
-    
     getResults();
 
 });
@@ -185,9 +210,10 @@ const api = {
     key: "334f64f5734edf8ec8c3143c2844165b",
     base: "https://api.openweathermap.org/data/2.5/"
 };
-let openWeatherUrl = `https://api.openweathermap.org/data/2.5/weather?q=Krakow&APPID=${api.key}`;
-function getResults(){
-    fetch("https://api.openweathermap.org/data/2.5/weather?q=Krakow&APPID=334f64f5734edf8ec8c3143c2844165b")
+async function getResults(){
+
+    city = document.querySelector("#noteTitle").value;
+    fetch(`https://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pl&APPID=334f64f5734edf8ec8c3143c2844165b`)
       
         .then(function(response){
             console.log(response);
@@ -195,19 +221,25 @@ function getResults(){
             return data;
         })
         .then(function(data){
-            
-            title= data.name;
-            window.contents = data.main.temp;
-            window.color="red";
+            console.log("dziala interwal");
+            // miasto , description, icon,temperature,pressure, humidity,color
+            miasto= "Miasto " + data.name + ", " + data.sys.country;
+            description ="Pogoda " + data.weather[0].description;
+            icon = data.weather[0].icon;
+            temperature ="Temperatura "+ parseFloat(data.main.temp-273.15).toFixed(1);
+            pressure ="Ciśnienie " + data.main.pressure;
+            humidity ="Widoczność " +  data.main.humidity;
         })
         .then(displayWeather);
+
 }
 
 function log(log){
     console.log(log);
 }
 function displayWeather(){
-    const n =  new Note(title,window.contents,true,window.color);
+    const iconIMG=`<img src="http://openweathermap.org/img/wn/${icon}@2x.png"/>`;
+    const n =  new Note(miasto , description, iconIMG,temperature,pressure, humidity, window.color);
     notes.addNote(n);
 }
 
