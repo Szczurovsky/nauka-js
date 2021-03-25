55841c21515244dc04155e5162cd09186cc42768
Dawid Szczurkowski <dszczurkowski@gmail.com>
2021-03-25 13:15:33 +0100

    pierwszy

---
+++ projekty_4_sem/aplikacje/lab/zad3/src/Board.ts
@@ -0,0 +1,34 @@
+import { Cell } from "./Cell";
+
+// class Board {
+//     cells: Cell[];
+//     constructor(size: number) {
+//         this.cells = new Array(size);
+//         let table = <HTMLTableElement>document.getElementById("tictactoe");
+//         let i = 0;
+//         for (let r = 0; r < size; r++) {
+//             let row = table.insertRow(r);
+//             for (let c = 0; c < size; c++) {
+//                 let cell = <HTMLTableDataCellElement>row.insertCell(c);
+//                 cell.className = "cell";
+//                 const newCell = new Cell(cell);
+//                 this.cells[i] = newCell;
+//                 cell.addEventListener(
+//                     "click",
+//                     () => this.makeMove(newCell),
+//                     false
+//                 );
+//                 i++;
+//             }
+//         }
+//     }
+//     makeMove(cell: Cell): void {
+//         cell.setCellValue(this.currentSymbol);
+//     }
+// }
+
+export class Board {
+    constructor(size: number) {
+        this.board = size;
+    }
+}
